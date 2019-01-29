4234f54e683ffd510287faa3e0b5713489d1c97c
Abhinuv Nitin Pitale <abhinuvpitale@users.noreply.github.com>
2019-01-29 09:41:43 -0500

    update commandline parameter for Raspberry Pi

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=OFF ../
+cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=OFF -DRASPBERRY=ON ../
 make thinger
 ./thinger
